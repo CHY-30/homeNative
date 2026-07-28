567ddea7ec20823180ff030661be14d74337fa68
CHY-30 <choshoo82@gmail.com>
2026-07-28 09:54:58 +0900

    member_b
    회원 레이아웃 작업

--- src/app/_LayoutIndex.tsx
+++ src/app/_LayoutIndex.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Footer from './footer';
-import Header from './header';
 
 interface PageLayoutProps {
   children: React.ReactNode; // 중단 영역 (FlatList, ScrollView, Custom View 등)
@@ -11,16 +9,11 @@ interface PageLayoutProps {
 export default function PageLayout({ children }: PageLayoutProps) {
   return (
     <SafeAreaView style={styles.container}>
-      {/* 1. 상단 컴포넌트 */}
-      <Header />
 
-      {/* 2. 중단 (오직 children만 위치하는 순수 영역) */}
       <View style={styles.body}>
         {children}
       </View>
 
-      {/* 3. 하단 컴포넌트 */}
-      <Footer />
     </SafeAreaView>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,33 @@
-import { ScrollView } from 'react-native';
-import AppText from '../components/AppText';
-import PageLayout from '../components/pageLayout';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import AppText from '../components/textAll';
+import PageLayout from './_LayoutIndex';
 
-export default function MyScreen() {
+export default function index() {
   return (
     <PageLayout>
       {/* ==================== 중단 영역 ==================== */}
-      <ScrollView contentContainerStyle={{ padding: 20 }}>
+      <ScrollView contentContainerStyle={{ padding: 20, flex: 1 }}>
+        <View style={styles.container}>
         <AppText size={22} weight="bold" color="#0F172A" style={{ marginBottom: 12 }}>
-          중단 메인 콘텐츠
+          홈택트
         </AppText>
 
         <AppText size={15} color="#475569" style={{ lineHeight: 22 }}>
-          상단(Header)과 하단(Footer)은 커스텀 UI 컴포넌트로 분리되어 상/하단에 고정되고,
-          중단은 ScrollView, FlatList 등 원하는 스크롤/레이아웃으로 자유롭게 컨트롤 가능합니다.
+          홈택트 설명..
         </AppText>
+        </View>
       </ScrollView>
       {/* =================================================== */}
     </PageLayout>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
